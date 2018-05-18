3e60f39526bc3fb972d0189bc8f7d026517b6693
Alex McKean <amckean@me.com>
2018-05-18 22:09:14 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -59,9 +59,10 @@ class TweetsController < ApplicationController
   delete '/tweets/:id/delete' do
     while session[:user_id]
       @tweet = Tweet.find_by_id(params[:id])
-        if @tweet.user_id == session[:user_id] 
+        if @tweet.user_id == session[:user_id]
           @tweet.delete
           redirect "/tweets"
+        else
         redirect "/tweets"
       end
         redirect "/login"
