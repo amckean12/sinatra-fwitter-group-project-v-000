06e5e39ef9b7636187ceb85b205bb0f84448bd61
Alex McKean <amckean@me.com>
2018-05-18 21:08:03 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,7 +16,7 @@ class TweetsController < ApplicationController
   post '/tweets' do
     if params[:content] == ""
       redirect to "/tweets/new"
-    else 
+    else
       user = User.find_by_id(session[:user_id])
       @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
       redirect "/tweets/#{@tweet.id}"
